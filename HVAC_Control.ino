a40bb13fd2f1874b8a34039316a7c96adc6d694b
Hamit-ibrahimoglu <230202915@ostimteknik.edu.tr>
2026-05-01 21:10:33 +0300

    sensors and motors pins setup

--- HVAC_Control.ino
+++ HVAC_Control.ino
@@ -1,1 +1,22 @@
+//Pins Configure
+const int tempPins[4] = {A0, A1, A2, A3};
+const int motorPins[4] = {3, 5, 6, 9};
 
+void setup() {
+  Serial.begin(9600);
+  
+  //motor pin
+  for(int i = 0; i < 4; i++) {
+    pinMode(motorPins[i], OUTPUT);
+  }
+}
+
+void loop() {
+  // Read the values from LM35 sensors
+  int rawTemp1 = analogRead(tempPins[0]);
+  int rawTemp2 = analogRead(tempPins[1]);
+  int rawTemp3 = analogRead(tempPins[2]);
+  int rawTemp4 = analogRead(tempPins[3]);
+  
+  delay(500);
+}
