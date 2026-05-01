18f9821502e10800358f4d211faa7c655c70ba43
Hamit-ibrahimoglu <230202915@ostimteknik.edu.tr>
2026-05-01 21:37:06 +0300

    Added readTemp function to convert analog signals into Celsius degrees

--- HVAC_Control.ino
+++ HVAC_Control.ino
@@ -5,18 +5,28 @@ const int motorPins[4] = {3, 5, 6, 9};
 void setup() {
   Serial.begin(9600);
   
-  //motor pin
+  //motor pins setup
   for(int i = 0; i < 4; i++) {
     pinMode(motorPins[i], OUTPUT);
   }
 }
 
+
+//ADC value to Celsius (Signal Scaling) convert Function
+float readTemp(int pin) {
+  int sensorVal = analogRead(pin);
+  //conversion: 5V / 1024 steps = 4.88mV per step
+  float voltage = (sensorVal * 5.0) / 1024.0;
+  return (voltage - 0.5) * 100.0;
+}
+
+
 void loop() {
-  // Read the values from LM35 sensors
-  int rawTemp1 = analogRead(tempPins[0]);
-  int rawTemp2 = analogRead(tempPins[1]);
-  int rawTemp3 = analogRead(tempPins[2]);
-  int rawTemp4 = analogRead(tempPins[3]);
+  //processe readings through the scaling function
+  float temp1 = readTemp(tempPins[0]);
+  float temp2 = readTemp(tempPins[1]);
+  float temp3 = readTemp(tempPins[2]);
+  float temp4 = readTemp(tempPins[3]);
   
   delay(500);
 }
