6e5da83456bc0adfd35d7020d225e47c016fa1b4
Hamit-ibrahimoglu <230202915@ostimteknik.edu.tr>
2026-05-07 13:10:54 +0300

    The last update (remove comments and organize the last steps)

--- HVAC_Control.ino
+++ HVAC_Control.ino
@@ -1,126 +1,120 @@
 #include <LiquidCrystal.h>
 
-// Moved d7 to pin 11 to free up pin 2 for the emergency interrupt
-const int rs = 13, en = 12, d4 = 8, d5 = 7, d6 = 4, d7 = 11;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+LiquidCrystal lcd(13, 12, 8, 7, 4, 11);
 
-const int tempPins[4] = {A0, A1, A2, A3};
-const int motorPins[4] = {3, 5, 6, 9};
+int tPins[] = {A0, A1, A2, A3};
+int mPins[] = {3, 5, 6, 9};
+int emBtn = 2;
+int tTemp = 24; 
+float corr = 0.5; 
 
-// Emergency button connected to Pin 2 for hardware interrupt support
-const int emergencyBtnPin = 2;
+volatile bool stopSys = false; 
 
-const int targetTemp = 24; 
-float correlation = 0.5; // Final tuning: increased correlation for faster response
-
-volatile bool isEmergency = false; 
-
-void emergency_isr() {
-  isEmergency = true;
+void halt_sys() {
+  stopSys = true;
 }
 
 void setup() {
   Serial.begin(9600);
-  
   for(int i = 0; i < 4; i++) {
-    pinMode(motorPins[i], OUTPUT);
+    pinMode(mPins[i], OUTPUT);
   }
-  
-  pinMode(emergencyBtnPin, INPUT_PULLUP); 
-  
-  attachInterrupt(digitalPinToInterrupt(emergencyBtnPin), emergency_isr, FALLING);
-  
+  pinMode(emBtn, INPUT_PULLUP); 
+  attachInterrupt(digitalPinToInterrupt(emBtn), halt_sys, FALLING);
   lcd.begin(16, 2);
 }
 
-float readTemp(int pin) {
-  int sensorVal = analogRead(pin);
-  float voltage = (sensorVal * 5.0) / 1024.0;
-  return (voltage - 0.5) * 100.0;
+float get_temp(int pin) {
+  analogRead(pin); 
+  delay(10);       
+  int val = analogRead(pin); 
+  float v = (val * 5.0) / 1024.0;
+  return v * 100.0; 
 }
 
-int calc_base_pwm(float temp) {
-  if (temp <= targetTemp) {
-    return 0; 
-  }
-  int speed = (temp - targetTemp) * 15; 
-  if (speed > 255) {
-    speed = 255;
-  }
-  return speed;
+int get_pwm(float t) {
+  if (t <= tTemp) return 0; 
+  int spd = (t - tTemp) * 15; 
+  if (spd > 255) spd = 255;
+  return spd;
 }
 
-void displayRoom(int roomIndex, int col, int row, float temp, int speedPercent) {
-  lcd.setCursor(col, row);
-  lcd.print(roomIndex + 1); 
+void print_room(int idx, int c, int r, float t, int p) {
+  lcd.setCursor(c, r);
+  lcd.print("T");
+  lcd.print(idx + 1); 
   lcd.print(":"); 
   
-  // Format temperature display
-  int tInt = (int)temp;
-  if (tInt < 10 && tInt >= 0) lcd.print("0"); 
-  lcd.print(tInt);
-  lcd.print("C "); 
+  int ti = (int)t;
+  if (ti < 10 && ti >= 0) lcd.print("0"); 
+  lcd.print(ti);
   
-  // Format speed display (00, MX, or 2 digits)
-  if (speedPercent <= 0) {
-    lcd.print("00 ");
-  } else if (speedPercent >= 100) {
-    lcd.print("MX ");
+  lcd.print("F"); 
+  
+  if (p <= 0) {
+    lcd.print("00");
+  } else if (p >= 100) {
+    lcd.print("MX");
   } else {
-    if (speedPercent < 10) lcd.print("0");
-    lcd.print(speedPercent);
-    lcd.print(" ");
+    if (p < 10) lcd.print("0");
+    lcd.print(p);
   }
 }
 
 void loop() {
-  if (isEmergency) {
+  if (stopSys) {
     for(int i = 0; i < 4; i++) {
-      analogWrite(motorPins[i], 0); 
+      analogWrite(mPins[i], 0); 
     }
     lcd.clear();
     lcd.setCursor(0, 0);
     lcd.print(" SYSTEM HALTED! ");
     lcd.setCursor(0, 1);
     lcd.print(" HARDWARE RESET ");
-    
-    while(true) {} 
+    Serial.println("EMERGENCY STOP!");
+    while(true); 
   }
 
-  float temp1 = readTemp(tempPins[0]);
-  float temp2 = readTemp(tempPins[1]);
-  float temp3 = readTemp(tempPins[2]);
-  float temp4 = readTemp(tempPins[3]);
+  float t1 = get_temp(tPins[0]);
+  float t2 = get_temp(tPins[1]);
+  float t3 = get_temp(tPins[2]);
+  float t4 = get_temp(tPins[3]);
   
-  int base1 = calc_base_pwm(temp1);
-  int base2 = calc_base_pwm(temp2);
-  int base3 = calc_base_pwm(temp3);
-  int base4 = calc_base_pwm(temp4);
+  int b1 = get_pwm(t1);
+  int b2 = get_pwm(t2);
+  int b3 = get_pwm(t3);
+  int b4 = get_pwm(t4);
 
-  int final1 = base1 + (correlation * base2);
-  int final2 = base2 + (correlation * base1) + (correlation * base3);
-  int final3 = base3 + (correlation * base2) + (correlation * base4);
-  int final4 = base4 + (correlation * base3);
+  int f1 = b1 + (corr * b2);
+  int f2 = b2 + (corr * b1) + (corr * b3);
+  int f3 = b3 + (corr * b2) + (corr * b4);
+  int f4 = b4 + (corr * b3);
 
-  if (final1 > 255) final1 = 255;
-  if (final2 > 255) final2 = 255;
-  if (final3 > 255) final3 = 255;
-  if (final4 > 255) final4 = 255;
+  if (f1 > 255) f1 = 255;
+  if (f2 > 255) f2 = 255;
+  if (f3 > 255) f3 = 255;
+  if (f4 > 255) f4 = 255;
 
-  analogWrite(motorPins[0], final1);
-  analogWrite(motorPins[1], final2);
-  analogWrite(motorPins[2], final3);
-  analogWrite(motorPins[3], final4);
+  analogWrite(mPins[0], f1);
+  analogWrite(mPins[1], f2);
+  analogWrite(mPins[2], f3);
+  analogWrite(mPins[3], f4);
   
-  int pct1 = map(final1, 0, 255, 0, 100);
-  int pct2 = map(final2, 0, 255, 0, 100);
-  int pct3 = map(final3, 0, 255, 0, 100);
-  int pct4 = map(final4, 0, 255, 0, 100);
+  int p1 = map(f1, 0, 255, 0, 100);
+  int p2 = map(f2, 0, 255, 0, 100);
+  int p3 = map(f3, 0, 255, 0, 100);
+  int p4 = map(f4, 0, 255, 0, 100);
 
-  displayRoom(0, 0, 0, temp1, pct1); 
-  displayRoom(1, 8, 0, temp2, pct2); 
-  displayRoom(2, 0, 1, temp3, pct3); 
-  displayRoom(3, 8, 1, temp4, pct4); 
+  print_room(0, 0, 0, t1, p1); 
+  print_room(1, 8, 0, t2, p2); 
+  print_room(2, 0, 1, t3, p3); 
+  print_room(3, 8, 1, t4, p4); 
   
-  delay(200);
+  Serial.print("T1:"); Serial.print((int)t1); Serial.print("C F1:"); Serial.print(p1); Serial.println("%");
+  Serial.print("T2:"); Serial.print((int)t2); Serial.print("C F2:"); Serial.print(p2); Serial.println("%");
+  Serial.print("T3:"); Serial.print((int)t3); Serial.print("C F3:"); Serial.print(p3); Serial.println("%");
+  Serial.print("T4:"); Serial.print((int)t4); Serial.print("C F4:"); Serial.print(p4); Serial.println("%");
+  Serial.println("-----------------");
+
+  delay(400); 
 }
